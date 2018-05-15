4db82b46fbbe66d0231d058ad3c72a748361eea8
Julio Valle <juliovr_93@hotmail.com>
2018-05-15 15:40:46 -0500

    Console

--- events.js
+++ events.js
@@ -3,7 +3,7 @@ module.exports = {
   client_events: (socket) => {
     
     socket.on('client_checked', data => {
-      console.log(data)
+      console.log(socket.id+':', data)
       socket.broadcast.emit('server_checked', data)
     })
 
